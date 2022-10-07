2447d78f897d2ac16c40bbbb9b563f010fed10b2
tayu31 <godslamdunk1423@icloud.com>
2022-10-07 16:59:13 +0900

    リモートリポジトリ

--- sampleEnv.py
+++ sampleEnv.py
@@ -7,3 +7,5 @@ CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+print("a")
