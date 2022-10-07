73792d042d30d7f6bddc2974e2589da3c624fdad
tayu31 <godslamdunk1423@icloud.com>
2022-10-07 15:14:00 +0900

    test

---
+++ sampleEnv.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+ 
+# 取得した各種キーを格納-----------------------------------------------------
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
